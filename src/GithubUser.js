1c2c0bc0a98ca8d4100908df6da0e1c039bb2a4a
Federico Ortiz <fede@stuffsandstuffs.coldspring.marian.edu>
2017-07-11 16:09:09 -0400

    Adding markup and styling for the user, and update user info when location changes

--- src/GithubUser.js
+++ src/GithubUser.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import './GithubUser.css'
 
 class GithubUser extends Component {
     constructor(props) {
@@ -8,17 +9,34 @@ class GithubUser extends Component {
             user: {}
         }
 
-        this.fetchUserData()
+        this.fetchUserData(props)
     }
 
-    fetchUserData = () => {
+    fetchUserData = (props) => {
         fetch(`https://api.github.com/users/${this.props.match.params.username}`)
             .then(response => response.json())
             .then(user => this.setState({ user }))
     }
 
+    componentWillReceiveProps(nextProps) {
+        const locationChanged = nextProps.location !== this.props.location
+        if (locationChanged) {
+            this.fetchUserData(nextProps)
+        } 
+    }
+
     render() {
-        return <h1>github user</h1>
+        const { user } = this.state
+        return (
+            <div className="github-user">
+                <img src={user.avatar_url} alt="github user avatar" />
+                <h2>{user.login}</h2>
+                <h3>followers: {user.followers}</h3>
+                <h3>following: {user.following}</h3>
+                <h3>location: {user.location}</h3>
+                <a href={user.html_url} target="_">Link to {user.login}'s profile</a>
+            </div>
+        )
     }
 }
 
